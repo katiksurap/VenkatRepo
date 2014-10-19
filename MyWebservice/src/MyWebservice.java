c29d06c7f6bdae7c1fc664678b3daf43db54042e
katiksurap <svr.surap@gmail.com>
2014-10-19 13:15:35 +0530

    Change of request

--- MyWebservice/src/MyWebservice.java
+++ MyWebservice/src/MyWebservice.java
@@ -4,7 +4,7 @@ public class MyWebservice {
 	
 	public String hello(String name){
 
-	    System.out.println("HelloService.hello(String name): value of name: " + name);
+	    System.out.println("HelloService.hello(String name): value of karthik: " + name);
 
 	    if( name == null || name.equals("") ){
 	        return "Hello World";
